d66172413ced93694d9fa2445177b37cd23ec77b
amishthapa <amish@smartcontents.com.np>
2025-10-07 08:31:55 +0545

    utility pages

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import About from './pages/about/index.jsx'
-import Home from './pages/home/index.jsx'
-
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import PrivacyPolicy from "./pages/privacy-policy/index.js";
+import NotFound from "./pages/not-found/NotFound";
 const AppRoutes = () => {
   return (
     <Routes>
-
-        <Route path="/" element={<Home />} />
-      
+      <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      {/* 404 Catch-all route */}
+      <Route path="*" element={<NotFound />} /> {/* ✅ Works now */}
     </Routes>
-  )
-}
+  );
+};
 
 export default AppRoutes;
