60c8ba257eedbf1a9d89212da323c11e1bfcf23e
Shital Prajapati <shital26prajapati@gmail.com>
2025-10-07 14:41:14 +0545

    added get in touch button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ export default function Navbar() {
               Contact
             </a>
             <button className="ml-4 px-4 py-2 rounded-lg bg-secondary-500 text-white hover:bg-secondary-400 transition">
-              Get Started
+              Get In Touch
             </button>
           </div>
 
