302c63b5fb534662ba92b3165601dbead457e688
Shital Prajapati <shital26prajapati@gmail.com>
2025-10-09 17:12:05 +0545

    added term of service page

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,12 +3,14 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import PrivacyPolicy from "./pages/privacy-policy/index.js";
 import NotFound from "./pages/not-found/NotFound";
+import TermsOfService from "./pages/term-of-service/index.jsx";
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      <Route path="/terms-of-service" element={<TermsOfService />} />
       {/* 404 Catch-all route */}
       <Route path="*" element={<NotFound />} /> {/* ✅ Works now */}
     </Routes>
