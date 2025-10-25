745fb570c3a5e177df26c68f62d25d560fdd18f5
amishthapa <amish@smartcontents.com.np>
2025-10-25 08:33:10 +0545

    spelling error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ export default function Navbar() {
 
   const dropdownLinks = [
     { icon: Flame, label: "Heating", to: "/services/heating" },
-    { icon: Factory, label: "Cmmercial Catering", to: "/services/catering" },
+    { icon: Factory, label: "Commercial Catering", to: "/services/catering" },
     { icon: Droplet, label: "Plumbing", to: "/services/plumbing" },
     { icon: Hammer, label: "Construction", to: "/services/construction" },
   ];
