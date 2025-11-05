e7ab2a5ca7e87b0133169c9af97a190645c33ba8
Shital Prajapati <shital26prajapati@gmail.com>
2025-11-05 19:38:19 +0545

    edited text

--- src/pages/home/hero-section.jsx
+++ src/pages/home/hero-section.jsx
@@ -151,7 +151,7 @@ const slides = [
     image:
       "https://plus.unsplash.com/premium_photo-1723823036427-b19e6d270bb6?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2070",
       link: "/services/catering",
-    title: "Professional Catering",
+    title: "Commercial Catering",
     alt: "Catering service",
   },
 ];
